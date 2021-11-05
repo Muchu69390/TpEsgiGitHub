b296467b198605580dd29de354f312a53984c647
Mathis Rome <mathis.rome@icloud.com>
2021-11-05 12:42:42 +0100

    remove description and title card

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.getElementById("add-card-button").addEventListener("click", function () {
     var cardcontainer = document.getElementById("card-container");
-    cardcontainer.innerHTML += '<div class="card" style="width: 24%">'+'<img class="card-img-top" src="https://source.unsplash.com/random" alt="Card image cap" >'+'<div class="card-body">'+'<h5 class="card-title">Top 10 des femmes les plus belles du monde</h5>'+' <p class="card-text">Découvrez notre séléction des 10 femmes les plus belles du monde, la 8ème va vous etonnez !</p>'+' <a href="#" class="btn btn-primary">Clique sur moi</a>'+'</div>'+'</div>'
+    cardcontainer.innerHTML += '<div class="card">'+'<img class="card-img-top" src="https://source.unsplash.com/random" alt="Card image cap" >'+'</div>'
 });
