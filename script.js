20bc84ce83aec7c7fa847c4752ba0f10991a403d
Mathis Rome <mathis.rome@icloud.com>
2021-11-05 12:51:41 +0100

    change automatic picture

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.getElementById("add-card-button").addEventListener("click", function () {
+
     var cardcontainer = document.getElementById("card-container");
-    cardcontainer.innerHTML += '<div class="card">'+'<img class="card-img-top" src="https://source.unsplash.com/random" alt="Card image cap" >'+'</div>'
+    cardcontainer.innerHTML += '<img class="card-img-top" src="https://source.unsplash.com/random/' + Math.floor(Math.random() * (800 - 600 + 1) + 600)+'x'+Math.floor(Math.random() * (800 - 600 + 1) + 600)+'" alt="Card image cap" >'
 });
